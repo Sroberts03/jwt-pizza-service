579fc6733368742bfd76e6a94380815e2927d8bb
Sam Roberts <samr72003@gmail.com>
2025-09-30 15:55:16 -0600

    feat(tests): add global setup and teardown for database management

---
+++ globalSetup.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+const config = require('./src/config.js');
+
+module.exports = async () => {
+  const connection = await mysql.createConnection({
+    host: config.db.connection.host,
+    user: config.db.connection.user,
+    password: config.db.connection.password,
+    multipleStatements: true,
+  });
+  await connection.query('CREATE DATABASE IF NOT EXISTS pizza_test');
+  await connection.query('USE pizza_test');
+  // You may want to run your schema setup here, e.g.:
+  // await connection.query('CREATE TABLE ...');
+  // Or import your schema from a file
+  connection.end();
+};
+
---
+++ globalTeardown.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+const config = require('./src/config.js');
+
+module.exports = async () => {
+  const connection = await mysql.createConnection({
+    host: config.db.connection.host,
+    user: config.db.connection.user,
+    password: config.db.connection.password,
+    multipleStatements: true,
+  });
+  await connection.query('DROP DATABASE IF EXISTS pizza_test');
+  connection.end();
+};
+
