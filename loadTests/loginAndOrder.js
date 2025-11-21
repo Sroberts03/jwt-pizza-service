7fdce759c74b70aa94635b363d3efa4f4e2d64c6
Sam Roberts <samr72003@gmail.com>
2025-11-20 21:36:29 -0700

    feat(loadTests): add load testing scenarios for login and order processes

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,282 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+    Imported_HAR: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 20, duration: '1m' },
+        { target: 20, duration: '3m30s' },
+        { target: 0, duration: '1m' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'imported_HAR',
+    },
+  },
+}
+
+// Scenario: Scenario_1 (executor: ramping-vus)
+
+export function scenario_1() {
+  let response
+
+  // Automatically added sleep
+  sleep(1)
+}
+
+// Scenario: Imported_HAR (executor: ramping-vus)
+
+export function imported_HAR() {
+  let response
+
+  const vars = {}
+
+  response = http.put(
+    'https://pizza-service.samrobertsqa.click/api/auth',
+    '{"email":"a@jwt.com","password":"admin"}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        origin: 'https://pizza.samrobertsqa.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+  if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Login was *not* 200');
+  }
+
+  vars['token'] = jsonpath.query(response.json(), '$.token')[0]
+
+  response = http.options('https://pizza-service.samrobertsqa.click/api/auth', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'content-type',
+      'access-control-request-method': 'PUT',
+      origin: 'https://pizza.samrobertsqa.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(38.4)
+
+  response = http.get('https://pizza-service.samrobertsqa.click/api/order/menu', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      origin: 'https://pizza.samrobertsqa.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  response = http.options('https://pizza-service.samrobertsqa.click/api/order/menu', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'GET',
+      origin: 'https://pizza.samrobertsqa.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  response = http.get(
+    'https://pizza-service.samrobertsqa.click/api/franchise?page=0&limit=20&name=*',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.samrobertsqa.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+
+  response = http.options(
+    'https://pizza-service.samrobertsqa.click/api/franchise?page=0&limit=20&name=*',
+    null,
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        origin: 'https://pizza.samrobertsqa.click',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+  sleep(4.8)
+
+  response = http.get('https://pizza-service.samrobertsqa.click/api/user/me', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      origin: 'https://pizza.samrobertsqa.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  response = http.options('https://pizza-service.samrobertsqa.click/api/user/me', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'GET',
+      origin: 'https://pizza.samrobertsqa.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(1.8)
+
+  response = http.post(
+    'https://pizza-service.samrobertsqa.click/api/order',
+    '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.samrobertsqa.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+
+  response = http.options('https://pizza-service.samrobertsqa.click/api/order', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'POST',
+      origin: 'https://pizza.samrobertsqa.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(1.9)
+
+  vars['jwtToken'] = jsonpath.query(response.json(), '$.jwt')[0]
+
+  response = http.post(
+    'https://pizza-factory.cs329.click/api/order/verify',
+    '{"jwt":"${vars[\'jwtToken\']}"}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.samrobertsqa.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-storage-access': 'active',
+      },
+    }
+  )
+
+  response = http.options('https://pizza-factory.cs329.click/api/order/verify', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'POST',
+      origin: 'https://pizza.samrobertsqa.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+  })
+}
